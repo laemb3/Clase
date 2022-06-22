b22e7a3246fb394dead18ae13200ab651ba94a6d
laemb3 <barreralae164@gmail.com>
2022-06-21 23:33:31 -0500

    practica10-11

---
+++ Practica11.py
@@ -0,0 +1,9 @@
+print ("Indicador de múltiplos")
+print ("Escribe un número:")
+multiplo = int(input())
+if multiplo % 2 == 0:
+    print("Es un multiplo de 2")
+if multiplo % 3 == 0:
+    print("Es un multiplo de 3")
+if multiplo % 5 == 0:
+    print("Es un multiplo de 5")
